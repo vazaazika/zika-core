1aa872dd94f9858951a4fdc03708d79cd79d60a7
Diego <diego.cedrim@gmail.com>
2018-04-09 20:28:07 -0300

    evita duplicacao no PoiStatusUpdate; o criador do time vira membro na hora da criação; Auth com case insensitive

--- src/main/java/br/les/opus/dengue/core/repositories/PoiStatusUpdateRepository.java
+++ src/main/java/br/les/opus/dengue/core/repositories/PoiStatusUpdateRepository.java
@@ -1,11 +1,26 @@
 package br.les.opus.dengue.core.repositories;
 
+import java.util.List;
+
+import org.hibernate.Query;
 import org.springframework.stereotype.Repository;
 
+import br.les.opus.auth.core.domain.User;
 import br.les.opus.commons.persistence.HibernateAbstractRepository;
 import br.les.opus.dengue.core.domain.PoiStatusUpdate;
+import br.les.opus.dengue.core.domain.PoiStatusUpdateType;
+import br.les.opus.dengue.core.domain.PointOfInterest;
 
 @Repository
 public class PoiStatusUpdateRepository extends HibernateAbstractRepository<PoiStatusUpdate, Long>{
 
+	@SuppressWarnings("unchecked")
+	public List<PoiStatusUpdate> findByUserAndPoi(User user, PointOfInterest poi, PoiStatusUpdateType type) {
+		String hql = "from PoiStatusUpdate where poi.id = :poiId and type.id = :tId and user.id = :uId ";
+		Query query = getSession().createQuery(hql);
+		query.setParameter("poiId", poi.getId());
+		query.setParameter("tId", type.getId());
+		query.setParameter("uId", user.getId());
+		return query.list();
+	}
 }
