8b28089b8dcb3fa200c0ff8f0f97897be6fff11d
anderson-uchoa <andersonguchoa@gmail.com>
2018-10-15 18:22:08 -0300

    improvement poistatus

--- src/main/java/br/les/opus/dengue/core/domain/enumeration/PoiStatus.java
+++ src/main/java/br/les/opus/dengue/core/domain/enumeration/PoiStatus.java
@@ -1,5 +1,20 @@
 package br.les.opus.dengue.core.domain.enumeration;
 
 public enum PoiStatus {
-    OPEN , IN_ANALYSIS, TREATED;
+    OPEN("Open"), IN_ANALYSIS("In analysis"), TREATED("Treated");
+
+    private String status;
+
+    PoiStatus(String status) {
+        this.status = status;
+
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
