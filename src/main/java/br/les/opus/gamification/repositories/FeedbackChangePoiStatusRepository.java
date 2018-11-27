f20c01711c261ab8df8f5b2c0977b083163aa027
anderson-uchoa <andersonguchoa@gmail.com>
2018-11-27 17:30:35 -0200

    add notation

--- src/main/java/br/les/opus/gamification/repositories/FeedbackChangePoiStatusRepository.java
+++ src/main/java/br/les/opus/gamification/repositories/FeedbackChangePoiStatusRepository.java
@@ -2,6 +2,8 @@ package br.les.opus.gamification.repositories;
 
 import br.les.opus.commons.persistence.HibernateAbstractRepository;
 import br.les.opus.gamification.domain.feedback.FeedbackChangePoiStatus;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class FeedbackChangePoiStatusRepository extends HibernateAbstractRepository<FeedbackChangePoiStatus, Long> {
 }
--- src/main/java/br/les/opus/gamification/repositories/FeedbackPoiInformationQualityRepository.java
+++ src/main/java/br/les/opus/gamification/repositories/FeedbackPoiInformationQualityRepository.java
@@ -2,6 +2,8 @@ package br.les.opus.gamification.repositories;
 
 import br.les.opus.commons.persistence.HibernateAbstractRepository;
 import br.les.opus.gamification.domain.feedback.FeedbackPoiInformationQuality;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class FeedbackPoiInformationQualityRepository extends HibernateAbstractRepository<FeedbackPoiInformationQuality, Long> {
 }
