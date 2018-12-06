f4ce1601b590b66adedba9d05e10fc42cf45e919
anderson-uchoa <andersonguchoa@gmail.com>
2018-12-06 13:50:28 -0200

    mundança para add agente da saude

--- src/main/java/br/les/opus/gamification/repositories/HealthAgentRepository.java
+++ src/main/java/br/les/opus/gamification/repositories/HealthAgentRepository.java
@@ -1,10 +1,8 @@
 package br.les.opus.gamification.repositories;
 
-import br.les.opus.auth.core.domain.User;
 import br.les.opus.commons.persistence.HibernateAbstractRepository;
 import br.les.opus.gamification.domain.HealthAgent;
-import org.hibernate.Criteria;
-import org.hibernate.criterion.Restrictions;
+
 import org.springframework.stereotype.Repository;
 
 @Repository
--- src/main/java/br/les/opus/gamification/services/HealthAgentService.java
+++ src/main/java/br/les/opus/gamification/services/HealthAgentService.java
@@ -1,15 +1,93 @@
 package br.les.opus.gamification.services;
 
 
+import br.les.opus.auth.core.domain.Resource;
+import br.les.opus.auth.core.domain.Role;
+import br.les.opus.auth.core.domain.User;
+import br.les.opus.auth.core.domain.UserRole;
+import br.les.opus.auth.core.repositories.ResourceRepository;
+import br.les.opus.auth.core.repositories.RoleRepository;
+import br.les.opus.auth.core.repositories.UserRepository;
+import br.les.opus.auth.core.repositories.UserRoleRepository;
+import br.les.opus.gamification.domain.HealthAgent;
+import br.les.opus.gamification.domain.Player;
 import br.les.opus.gamification.repositories.HealthAgentRepository;
+import br.les.opus.gamification.repositories.InviteRepository;
+import br.les.opus.gamification.repositories.ResetPasswordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 @Service
 public class HealthAgentService {
 
     @Autowired
     private HealthAgentRepository healthAgentRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleDao;
+
+    @Autowired
+    private Environment env;
+
+    @Autowired
+    private UserRoleRepository userRoleDao;
+
+    @Autowired
+    private ResourceRepository resourceDao;
+
+    /**
+     * Creates a new user along with its default roles
+     * @param user
+     * @return
+     */
+    public HealthAgent save(HealthAgent user) {
+        HealthAgent newUser = userRepository.save(user);
+        loadDefaultRoles(newUser);
+        //generateInviteID(newUser);
+        return newUser;
+    }
+
+    /**
+     * Adds the default roles to a specified user
+     * @param user
+     */
+    public void loadDefaultRoles(User user) {
+        List<Role> defaultRoles = this.getDefaultRoles();
+        for (Role role : defaultRoles) {
+            UserRole userRole = new UserRole();
+            userRole.setRole(role);
+            userRole.setUser(user);
+            userRoleDao.save(userRole);
+        }
+    }
+
+    private List<Role> getDefaultRoles() {
+        String rolesList = env.getProperty("user.roles.healthagent");
+        StringTokenizer tokenizer = new StringTokenizer(rolesList);
+        List<Role> roles = new ArrayList<>();
+        while (tokenizer.hasMoreTokens()) {
+            Role role = roleDao.findByAuthority(tokenizer.nextToken());
+            roles.add(role);
+        }
+        return roles;
+    }
+
+    public HealthAgent loadRolesAndResorces(HealthAgent user) {
+        List<Role> roles = roleDao.findAllByUser(user);
+        user.setRoles(roles);
+
+        List<Resource> resources = resourceDao.findAllByUser(user);
+        user.setResources(resources);
+        return user;
+    }
 
 
 }
